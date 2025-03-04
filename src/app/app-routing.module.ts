8113354df9bbef91fb75467c928665240a1b8b92
joao neivam neto <joaoneivanetos@gmail.com>
2025-03-04 14:52:40 -0300

    top da página lista de cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './Cadastrar/Cadastrar.component';
 import { PaginaIncialComponent } from './PaginaIncial/PaginaIncial.component';
+import { ListaCursosComponent } from './listaCursos/listaCursos.component';
 
 const routes: Routes = [
   { path:"cadastrar",component:CadastrarComponent},
   { path:"home", component:PaginaIncialComponent},
+  {path:"listaCursos", component:ListaCursosComponent},
   {path:"", redirectTo:"home",pathMatch:"full"}
 ];
 
