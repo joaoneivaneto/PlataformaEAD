d16b6827b48bac2acb30fa555ab5394ca2b1de88
joao neivam neto <joaoneivanetos@gmail.com>
2025-03-17 14:07:22 -0300

    página do login pronta

--- src/app/listaCursos/listaCursos-routing.module.ts
+++ src/app/listaCursos/listaCursos-routing.module.ts
@@ -1,13 +1,14 @@
-import { Route, RouterModule, Routes } from "@angular/router";
-import { ListaCursosComponent } from "../listaCursos.component";
-import { DetalheCursoComponent } from "./detalheCurso.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ListaCursosComponent } from './listaCursos.component';
+import { DetalheCursoComponent } from './detalheCurso/detalheCurso.component';
 import { NgModule } from "@angular/core";
+import {LoginComponent} from "../login/login.component";
 
 const routes:Routes = [
     { path:'', component:ListaCursosComponent},
-    {path:'detalhesCurso', component:DetalheCursoComponent},
-    {path:'login',component:LoginComponent})},
-];
+    {path:'login',component:LoginComponent},
+    {path:'detalhesCurso', component:DetalheCursoComponent}
+]
 
 @NgModule({
     imports:[RouterModule.forChild(routes)],
