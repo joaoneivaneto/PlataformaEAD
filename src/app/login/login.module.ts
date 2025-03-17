4d0897bd156666de9e2f34dd8f4d65cdab8badba
joao neivam neto <joaoneivanetos@gmail.com>
2025-03-17 09:44:36 -0300

    corrindo posicionamento dos cards da listacursos e do icone de busco do input

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent],
+  imports:[
+    CommonModule,
+    LoginRoutingModule,
+  ]
 })
 export class LoginModule { }
